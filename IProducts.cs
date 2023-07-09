712e6d11a51ca2ae5867cb0fbdd907910ed2afe0
sahba11 <92772701+sahba11@users.noreply.github.com>
2023-07-09 09:17:19 +0330

    Add files via upload

---
+++ IProducts.cs
@@ -0,0 +1,15 @@
+﻿
+using CrudApiJw.Models;
+namespace CrudApiJw.Interface
+{
+    public interface IProducts
+    {
+        public List<Product> GetProductDetails();
+        public Product GetProductDetails(int id);
+        public void AddProduct(Product product);
+        public void UpdateProduct(Product product);
+        public Product DeleteProduct(int id);
+        public bool CheckProduct(int id);
+        //object Get_dbContext();
+    }
+}
