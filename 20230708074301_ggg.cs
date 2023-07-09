3b1eacf9294c4190aa50215bf4428c9f442ebb6c
sahba11 <92772701+sahba11@users.noreply.github.com>
2023-07-09 09:18:20 +0330

    Add files via upload

---
+++ 20230708074301_ggg.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudApiJw.Migrations
+{
+    /// <inheritdoc />
+    public partial class ggg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ManufacturePhone = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ManufactureEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IsAvailable = table.Column<string>(type: "varchar(1)", unicode: false, maxLength: 1, nullable: true),
+                    ProduceDate = table.Column<DateTime>(type: "datetime2", unicode: false, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DisplayName = table.Column<string>(type: "varchar(60)", unicode: false, maxLength: 60, nullable: true),
+                    UserName = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    Email = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Password = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", unicode: false, nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
